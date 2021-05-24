75c74e0d29724c8ebb5ca4cfa39d8a9d8d15b3c1
Austin Houchens <austinhouchens1510@gmail.com>
2021-05-23 22:30:07 -0400

    added helper functions displayWelcomeMessage(), displayChoices(), and displayWinner(). Finished main logic of game with game()

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-function game() {
+game();
 
+function game() {
+    let cpuScore = 0,
+        playerScore = 0,
+        winner = "",
+        cpuChoice = "",
+        playerChoice = "";
+    
+    displayWelcomeMessage();
+    while (playerScore < 5 && cpuScore < 5) {
+        displayScore(cpuScore, playerScore);
+        cpuChoice = getComputerChoice();
+        playerChoice = getPlayerChoice();
+        displayChoices(cpuChoice, playerChoice);
+        winner = checkWinner(cpuChoice, playerChoice);
+        if (winner === "tie") console.log("It's a tie! Try again.");
+        else if (winner === "cpu") {
+            console.log(`CPU wins! ${cpuChoice} beats ${playerChoice}`);
+            ++cpuScore;
+        }
+        else {
+            console.log(`You win! ${playerChoice} beats ${cpuChoice}`);
+            ++playerScore;
+        }
+    }
+    displayWinner(cpuScore, playerScore);
+}
+
+function displayWelcomeMessage() {
+    console.log("Welcome to Rock Paper Scissors!\nYou will play against a computer.\nThe first to reach a score of 5 wins. Good luck!");
+}
+
+function displayScore(cpuScore, playerScore) {
+    console.log(`CPU Score: ${cpuScore}`);
+    console.log(`Your Score: ${playerScore}`);
 }
 
 function getComputerChoice() {
@@ -26,6 +60,11 @@ function validatePlayerChoice(c) {
         c = prompt("Make your choice: ").toLowerCase();
     }
     return c;
+}
+
+function displayChoices(cpuChoice, playerChoice) {
+    console.log(`CPU chose: ${cpuChoice}`);
+    console.log(`You chose: ${playerChoice}`);
 }
 
 function checkWinner(cpuChoice, playerChoice) {
@@ -64,5 +103,8 @@ function isTied(cpuChoice, playerChoice) {
     return (cpuChoice === playerChoice);
 }
 
-//console.log(`CPU wins! ${cpuChoice} beats ${playerChoice}`);
-//console.log(`You win! ${playerChoice} beats ${cpuChoice}`);
+function displayWinner(cpuScore, playerScore) {
+    if (cpuScore === 5) console.log("Drats! You lost :(");
+    else console.log("Congratulations! You won!");
+    console.log("Refresh the page to play again.");
+}
